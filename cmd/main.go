2320bc7ed362f018f404ac8599f817ba8bfed81f
sysatom <sysatom@gmail.com>
2020-10-14 14:16:03 +0800

    feat: Load balance

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"github.com/sysatom/rpc"
+	"github.com/sysatom/rpc/xclient"
 	"log"
 	"net"
-	"net/http"
 	"sync"
 	"time"
 )
@@ -19,33 +19,75 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addrCh chan string) {
-	var foo Foo
-	l, _ := net.Listen("tcp", ":5000")
-	_ = rpc.Register(&foo)
-	rpc.HandleHTTP()
-	addrCh <- l.Addr().String()
-	_ = http.Serve(l, nil)
+func (f Foo) Sleep(args Args, reply *int) error {
+	time.Sleep(time.Second * time.Duration(args.Num1))
+	*reply = args.Num1 + args.Num2
+	return nil
 }
 
-func call(addrCh chan string) {
-	client, _ := rpc.DialHTTP("tcp", <-addrCh)
-	defer func() { _ = client.Close() }()
+func startServer(addrCh chan string) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":0")
+	server := rpc.NewServer()
+	_ = server.Register(&foo)
+	addrCh <- l.Addr().String()
+	server.Accept(l)
+}
 
-	time.Sleep(time.Second)
+func foo(xc *xclient.XClient, ctx context.Context, typ, serviceMethod string, args *Args) {
+	var reply int
+	var err error
+	switch typ {
+	case "call":
+		err = xc.Call(ctx, serviceMethod, args, &reply)
+	case "broadcast":
+		err = xc.Broadcast(ctx, serviceMethod, args, &reply)
+	}
+	if err != nil {
+		log.Printf("%s %s error: %v", typ, serviceMethod, err)
+	} else {
+		log.Printf("%s %s success: %d + %d = %d", typ, serviceMethod, args.Num1, args.Num2, reply)
+	}
+}
+
+func call(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RoundRobinSelect, nil)
+	defer func() { _ = xc.Close() }()
 
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := &Args{Num1: i, Num2: i * i}
-			ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-			var reply int
-			if err := client.Call(ctx, "Foo.Sum", args, &reply); err != nil {
-				log.Fatal("call Foo.Sum error:", err)
-			}
-			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+			foo(xc, context.Background(), "call", "Foo.Sum", &Args{
+				Num1: i,
+				Num2: i * i,
+			})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func broadcast(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{
+				Num1: i,
+				Num2: i * i,
+			})
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+			foo(xc, ctx, "broadcast", "Foo.Sleep", &Args{
+				Num1: i,
+				Num2: i * i,
+			})
 		}(i)
 	}
 	wg.Wait()
@@ -53,7 +95,16 @@ func call(addrCh chan string) {
 
 func main() {
 	log.SetFlags(0)
-	addr := make(chan string)
-	go call(addr)
-	startServer(addr)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go startServer(ch1)
+	go startServer(ch2)
+
+	addr1 := <-ch1
+	addr2 := <-ch2
+
+	time.Sleep(time.Second)
+	call(addr1, addr2)
+	broadcast(addr1, addr2)
 }
