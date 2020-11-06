61b2953d300059c9c2fd33912a7f6751b390bd24
sysatom <sysatom@gmail.com>
2020-11-06 14:50:28 +0800

    feat: json codec

--- codec/codec.go
+++ codec/codec.go
@@ -31,4 +31,5 @@ var NewCodecFuncMap map[Type]NewCodecFunc
 func init() {
 	NewCodecFuncMap = make(map[Type]NewCodecFunc)
 	NewCodecFuncMap[GobType] = NewGobCodec
+	NewCodecFuncMap[JsonType] = NewJsonCodec
 }
